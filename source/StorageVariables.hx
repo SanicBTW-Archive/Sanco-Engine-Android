492bb3c39cedb26d02519896d84f0829ff8948dc
SanicBTW <sansunder718jqda@gmail.com>
2022-02-23 09:29:29 -0800

    images coming

--- source/StorageVariables.hx
+++ source/StorageVariables.hx
@@ -10,10 +10,14 @@ import haxe.io.Path;
 class StorageVariables
 {
 	public static var RequiredPath:String = Path.join([System.userDirectory, 'sanicbtw_psychfiles']);
-	public static var DataRPath:String = Path.join([RequiredPath, 'data']);
+	public static var DataPath:String = Path.join([RequiredPath, 'data']);
+    public static var ImagesPath:String = Path.join([RequiredPath, 'images']);
+    public static var CharactersPath:String = Path.join([RequiredPath, 'characters']);
 
     public static function CheckStuff() {
         if(!FileSystem.exists(RequiredPath)){FileSystem.createDirectory(RequiredPath);}
-        if(!FileSystem.exists(DataRPath)){FileSystem.createDirectory(DataRPath);}
+        if(!FileSystem.exists(DataPath)){FileSystem.createDirectory(DataPath);}
+        if(!FileSystem.exists(ImagesPath)){FileSystem.createDirectory(ImagesPath);}
+        if(!FileSystem.exists(CharactersPath)){FileSystem.createDirectory(CharactersPath);}
     }
 }

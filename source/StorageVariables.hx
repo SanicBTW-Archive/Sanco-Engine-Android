db1e34c3ad899ef0bb4cb5ef3d6a2ba03fdb9d6e
SanicBTW <sansunder718jqda@gmail.com>
2022-02-28 06:21:48 -0800

    fuck everything

--- source/StorageVariables.hx
+++ source/StorageVariables.hx
@@ -6,16 +6,13 @@ import sys.FileSystem;
 import lime.system.System;
 import haxe.io.Path;
 
-//this method will be deprecated once i find a good way to do it lol (expect to be on 0.3 or 0.4)
 class StorageVariables
 {
     
 	public static var RequiredPath:String = Path.join([System.userDirectory, 'sanicbtw_psychfiles']);
-    public static var ExportsPath:String = Path.join([RequiredPath, 'exports']);
 
     public static function CheckStuff() {
         if(!FileSystem.exists(RequiredPath)){FileSystem.createDirectory(RequiredPath);}
-        if(!FileSystem.exists(ExportsPath)){FileSystem.createDirectory(ExportsPath);}
 
     }
 }
